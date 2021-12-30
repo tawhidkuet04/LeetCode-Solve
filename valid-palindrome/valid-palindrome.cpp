042134f61843137ed051f29e3c5090106fcb9044
tawhidkuet04 <tawhidkuet04@gmail.com>
2021-12-31 01:23:05 +0600

    Time: 10 ms (12.49%), Space: 7.2 MB (88.83%) - LeetHub

--- valid-palindrome/valid-palindrome.cpp
+++ valid-palindrome/valid-palindrome.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     
-    char makeLowerCase(char c){
-        if(c >= 'A' && c <= 'Z'){
-            c = c - 'A' + 'a';
-        }
-        return c;
-    }
-    bool checkIfValidCharecter(char lowerChar){
-        lowerChar = makeLowerCase(lowerChar);
-        if( (lowerChar >= '0' && lowerChar <= '9') || (lowerChar >= 'a' && lowerChar <= 'z')){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    
     bool isPalindrome(string s) {
         int size = s.size();
         int left = 0, right = size -1 ;
         while( left < right){
-            if( !checkIfValidCharecter(s[left])){
+            if( !isalnum(s[left])){
                 left ++;
                 continue;
             }
-            if(!checkIfValidCharecter(s[right])){
+            if(!isalnum(s[right])){
                 right --;
                 continue;
             }
-            if( makeLowerCase(s[left]) == makeLowerCase(s[right])){
+            if(tolower(s[left]) == tolower(s[right])){
                 left ++ ;
                 right --;
             }else{
