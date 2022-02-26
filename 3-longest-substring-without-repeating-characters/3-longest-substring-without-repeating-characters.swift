3149c6513149816a0c794ff9ede2c8467227bd40
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-02-27 02:47:40 +0600

    Time: 24 ms (61.46%), Space: 14.7 MB (35.97%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.swift
@@ -0,0 +1,33 @@
+class Solution {
+    
+    func isRepeated(_ mp: [Character: Int]) -> Bool{
+        for (key, val) in mp{
+            if val > 1 {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var inputArray = Array(s), windowStart = 0, mx = 0
+        
+        var mp = [Character: Int]()
+        
+        for windowEnd in 0..<inputArray.count{
+            mp[inputArray[windowEnd], default: 0 ] += 1
+            
+            if !isRepeated(mp){
+               mx = max(mx, windowEnd - windowStart + 1) 
+            }else{
+                while( isRepeated(mp) ){
+                    mp[inputArray[windowStart], default: 0] -= 1
+                    windowStart += 1
+                }
+            }
+        }
+        
+        return mx
+        
+    }
+}
