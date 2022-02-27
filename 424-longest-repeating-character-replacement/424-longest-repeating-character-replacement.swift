cffbcf6d00d54d3d1c65e45de38b5e7c74301db1
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-02-28 03:29:57 +0600

    Time: 17 ms (87.23%), Space: 14.8 MB (13.83%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.swift
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.swift
@@ -1,26 +1,18 @@
 class Solution {
     
-    func getReplacebleCharCount(_  dict:  [Character: Int], _ start: Int, _ end: Int)  -> Int{
-        var mx = 0 
-        for (key, val) in dict{
-            mx = max(mx, val)
-        }
-        
-        return (end - start + 1 ) - mx 
-    }
-    
     func characterReplacement(_ s: String, _ k: Int) -> Int {
-        var windowStart = 0, mx = 0
+        var windowStart = 0, mx = 0, maxChar = 0
         var inputString = Array(s)
         var dict = [Character: Int]()
         
         for windowEnd in 0..<inputString.count{
             dict[inputString[windowEnd], default: 0 ] += 1
-            let replaceCharCount = getReplacebleCharCount(dict, windowStart, windowEnd)
+            maxChar = max(maxChar, dict[inputString[windowEnd], default: 0])
+            let replaceCharCount = (windowEnd - windowStart + 1) - maxChar
             if replaceCharCount <= k{
                 mx = max(mx, windowEnd - windowStart + 1 )
             }else{
-                while(getReplacebleCharCount(dict, windowStart, windowEnd) > k){
+                while( ((windowEnd - windowStart + 1) - maxChar) > k){
                     dict[inputString[windowStart], default: 0 ] -= 1
                     windowStart += 1
                 }
