b9a451cafd694cecd4ead25ab5b625bd93902eb3
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-02-28 16:34:18 +0600

    Time: 224 ms (80.89%), Space: 15.8 MB (12.84%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.swift
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.swift
@@ -1,51 +1,19 @@
 class Solution {
     func sortedSquares(_ nums: [Int]) -> [Int] {
-        var ans = [Int]()
-        if nums[0] >= 0 {
-            for num in nums{
-                ans.append(num * num)
-            }
-            return ans
-        }
+        var ans = Array(repeating: 0, count: nums.count)
         
-        if nums[nums.count - 1 ] < 0 {
-           for num in nums{
-                ans.append(num * num)
-            }
-            ans.reverse()
-            return ans
-        }
+        var left = 0, right = nums.count - 1, size = nums.count - 1
         
-        var leftP = 0, rightP = 0
-        
-        for index in 0..<nums.count{
-            if (nums[index] >= 0) {
-                leftP = index - 1
-                rightP = index 
-                break
-            }
-        }
-        
-        while( leftP >= 0 && rightP < nums.count){
-            let leftValue =  nums[leftP] * nums[leftP]
-            let rightValue = nums[rightP] * nums[rightP]
-            
-            if(leftValue >= rightValue){
-                ans.append( rightValue )
-                rightP += 1
+        for index in stride(from: size, through: 0, by: -1){
+            var square = 0 
+            if(abs(nums[left]) > abs(nums[right])){
+                square = nums[left]
+                left += 1
             }else{
-                ans.append( leftValue )
-                leftP -= 1
+                square = nums[right]
+                right -= 1
             }
-        }
-        
-        while(leftP >= 0){
-            ans.append(nums[leftP] * nums[leftP])
-            leftP -= 1
-        }
-        while(rightP < nums.count){
-            ans.append(nums[rightP] * nums[rightP])
-            rightP += 1
+            ans[index] = square * square
         }
         
         return ans
