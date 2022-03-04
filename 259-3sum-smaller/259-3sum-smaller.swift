a2792cd8abb347c41a6c197d55374087176f9ecf
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-05 02:20:06 +0600

    Time: 37 ms (41.38%), Space: 14.1 MB (82.76%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.swift
+++ 259-3sum-smaller/259-3sum-smaller.swift
@@ -1,52 +1,28 @@
 class Solution {
-    func binSearch(_ lo: Int, _ hi: Int, _ target: Int, _ nums: [Int]) -> Int{
-        var mid = 0
-        var index = lo
-        var lo =  lo, hi = nums.count - 1
-        while(lo < hi){
-            mid = (lo + hi + 1)/2
-            if (nums[mid] >= target){
-               
-                hi = mid - 1 
-            }
-            else{
-                index = mid
-                lo = mid 
-            }
-            
-        }
-        
-        return index
-        
-    }
-    // 2 3 4 6 
+    
     func threeSumSmaller(_ nums: [Int], _ target: Int) -> Int {
-        var arr = nums, ans = 0
+        var ans = 0, arr = nums
         arr.sort()
+
         if nums.count < 3{
             return 0
         }
-        for i in 0..<(arr.count - 2){
-            for j in (i+1)..<(arr.count - 1){
-                let sum = arr[i] + arr[j]
-                let dif = target - sum
-                
-                var range = binSearch(j, arr.count, dif, arr)
+        for i in 0..<arr.count-2{
+           var en = arr.count - 1
+           for j in (i+1)..<arr.count-1{
+               var sum = arr[i] + arr[j]
+               while ((sum + arr[en]) >= target && en > 0){
+                   en -= 1
+               }
+               // print("\(en) \(j)")
+               if en > j{
+                   ans += en - j
+               }
                
-                ans += range - j
-                
-                
-                // 1 2 3 
-                 
-                // if range == nums.count - 1 &&  nums[range] < dif {
-                //     ans += 1
-                // }
-                
-                
-            }
+           }
+            
         }
-        
         return ans
-
+        
     }
 }
