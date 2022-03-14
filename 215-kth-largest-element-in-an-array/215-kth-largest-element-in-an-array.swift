c6fc8d15b26acf3168a0425df2427d8e4c41cf57
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-14 20:10:34 +0600

    Time: 96 ms (23.67%), Space: 14.3 MB (50.44%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.swift
@@ -0,0 +1,51 @@
+class Solution {
+    var ans = 0
+    func partition(_ nums: inout [Int], _ left: Int, _ right: Int) -> Int{
+        
+        var pIndex = left
+        var pivot = right
+        
+        for index in left..<right{
+            if nums[index] <= nums[pivot]{
+                nums.swapAt(index, pIndex)
+                pIndex += 1
+            }
+        }
+        nums.swapAt(pivot, pIndex)
+        return pIndex
+  
+        
+    }
+    
+    func quickSelect(_ nums: inout [Int], _ left: Int, _ right: Int, _ k: Int){
+        if left >= right{
+            return
+        }
+        
+        var pivot = (left + right)/2
+        var index = partition( &nums, left, right )
+    
+        if index == (nums.count - k){
+             ans = index
+             return
+        }
+        
+        // if index < (nums.count - k){
+            quickSelect(&nums, index, right, k)
+            
+        // }else if index > (nums.count - k){
+            quickSelect(&nums, left, index - 1, k)
+        // }
+      
+    }
+    
+    func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+        var arr = nums
+        quickSelect( &arr, 0, nums.count - 1, k)
+        print(arr)
+        return arr[ans] 
+    }
+    
+    
+   
+}
