d9ce04cd830792666bc7712c8afa0e982cfef5a9
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-15 22:59:12 +0600

    Time: 38 ms (22.69%), Space: 13.9 MB (87.03%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.swift
+++ 162-find-peak-element/162-find-peak-element.swift
@@ -3,20 +3,19 @@ class Solution {
     var ans = 0
     func findPeak(_ arr: [Int], _ left: Int, _ right: Int){
       
-        // if left >= right{
-        //     return 
-        // }
-        if left < right{
-            var mid = (left + right) / 2
-
-            if  arr[mid + 1] > arr[mid]{
-                 ans = mid + 1
-                 findPeak(arr , mid + 1 , right)
-            }else  {
-                 findPeak(arr, left , mid)
-            } 
-            
+        if left >= right{
+            return 
         }
+        
+        var mid = (left + right) / 2
+        
+        if  arr[mid + 1] > arr[mid]{
+             ans = mid + 1
+             findPeak(arr , mid + 1 , right)
+        }else  {
+             findPeak(arr, left , mid)
+        } 
+        
         
         
     }
