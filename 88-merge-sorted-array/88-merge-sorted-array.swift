b4c0cd521fec236d6d481fb4ac1c4f79a5d935f5
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-18 01:35:39 +0600

    Time: 9 ms (64.37%), Space: 14.1 MB (65.75%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.swift
+++ 88-merge-sorted-array/88-merge-sorted-array.swift
@@ -1,19 +1,7 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
         var p1 = m - 1 , p = (m + n) - 1 , p2 = n - 1
-        
-        // if nums1.count == m{
-        //     return
-        // }
-        // if m == 0{
-        //     p = 0 ; p2 = 0
-        //     while(p2 < n){
-        //         nums1[p] = nums2[p2]
-        //         p2 += 1
-        //         p += 1
-        //     }
-        //     return
-        // }
+
         while( p1 >= 0 && p2 >= 0){
             if nums1[p1] > nums2[p2]{
                 nums1[p] = nums1[p1]
