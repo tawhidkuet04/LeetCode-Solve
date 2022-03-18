f93646ada6a4535cfa2571a22a02c226824230e8
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-18 15:56:36 +0600

    Time: 166 ms (14.48%), Space: 16 MB (67.76%) - LeetHub

---
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.swift
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+
+    var ans = Int.min
+
+    func maxSum(_ root: TreeNode?) -> Int{
+        if root == nil{
+            return 0
+        }
+
+        var left = root!.val + maxSum(root?.left)
+        var right =  root!.val + maxSum(root?.right)
+       
+        ans = max( ans, max( root!.val, max( max(left, right) , (left + right) - root!.val)))
+        print("\(root!.val)  \(left) ----- \(right)    \(ans)")
+        return max(max(left, right), root!.val)
+        
+    }
+    func maxPathSum(_ root: TreeNode?) -> Int {
+         maxSum(root)
+        return ans
+    }
+}
