19610c8a584b37d8bc3cb1c0c5831bf4f357e183
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-19 01:03:43 +0600

    Time: 0 ms (100.00%), Space: 14.4 MB (43.72%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.swift
+++ 20-valid-parentheses/20-valid-parentheses.swift
@@ -2,17 +2,18 @@ class Solution {
     func isValid(_ s: String) -> Bool {
         var stack = [Character]()
         var sArr = Array(s)
+        var mp = [Character: Character]()
+
         
+        mp[")"] = "("
+        mp["}"] = "{"
+        mp["]"] = "["
         
         for char in sArr{
             if char == "(" || char == "{" || char == "["{
                 stack.append(char)
-            }else if char == ")" && !stack.isEmpty && stack.last! == "("{ 
-                 stack.removeLast()
-            }else if char == "]" && !stack.isEmpty && stack.last! == "["{ 
-                 stack.removeLast()
-            }else if char == "}" && !stack.isEmpty && stack.last! == "{"{ 
-                 stack.removeLast()
+            }else if !stack.isEmpty && stack.last! == mp[char]{
+                stack.removeLast()
             }else{
                 return false
             }
