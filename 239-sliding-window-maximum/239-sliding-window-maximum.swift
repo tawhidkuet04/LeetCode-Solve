be980f309642bd60388b0801be386e8bfe68145e
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-03-18 13:06:49 +0600

    Time: 2804 ms (49.08%), Space: 25.9 MB (33.33%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.swift
+++ 239-sliding-window-maximum/239-sliding-window-maximum.swift
@@ -1,29 +1,26 @@
 class Solution {
+    
+    func cleanDeque(_ deque: inout [Int], _ index: Int, _ k : Int, _ nums: [Int]){
+        if !deque.isEmpty && deque.first! <= index - k{
+                deque.removeFirst()
+            }
+            
+            while( !deque.isEmpty && nums[deque.last!] < nums[index]){
+                deque.removeLast()
+            }
+    }
     func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
         
         var deque = [Int]()
         var ans = [Int]()
         for index in 0..<k-1{
-            if !deque.isEmpty && deque.first! <= index - k{
-                deque.removeFirst()
-            }
             
-            while( !deque.isEmpty && nums[deque.last!] < nums[index]){
-                deque.removeLast()
-            }
-            
+            cleanDeque( &deque, index, k, nums)
             deque.append(index)
         }
         for index in (k-1)..<nums.count{
             
-            if !deque.isEmpty && deque.first! <= index - k{
-                deque.removeFirst()
-            }
-            
-            while( !deque.isEmpty && nums[deque.last!] < nums[index]){
-                deque.removeLast()
-            }
-            
+            cleanDeque( &deque, index, k, nums)
             deque.append(index)
             ans.append(nums[deque.first!])
             
