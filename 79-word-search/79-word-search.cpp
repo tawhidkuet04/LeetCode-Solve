4f52e1c516b32d40da05b4e85ccd636601f25c27
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-17 02:47:49 +0600

    Time: 307 ms (77.72%), Space: 8.1 MB (36.51%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,13 +1,13 @@
 
 int fx[] = {0, 0, +1, -1};
 int fy[] = {+1, -1, 0, 0};
-int visited[7][7] = {0};
+
 class Solution {
 public:
     
     
     
-    bool wordSearch(int row, int col, int charIndex, string word, vector<vector<char>>& board ){
+    bool wordSearch(int row, int col, int charIndex, string word, vector<vector<char>>& board, int visited[7][7]){
         if (charIndex >= word.size()){
             return true;
         }
@@ -20,7 +20,7 @@ public:
            
             if ( (newRow >= 0 && newRow < board.size()) && (newCol >= 0 && newCol < board[0].size()) && board[newRow][newCol] == word[charIndex] && !visited[newRow][newCol]){
                 visited[newRow][newCol] = 1;
-                isFound |= wordSearch(newRow, newCol, charIndex + 1, word, board );
+                isFound |= wordSearch(newRow, newCol, charIndex + 1, word, board , visited);
                 if(isFound){
                     return true;
                 }
@@ -33,13 +33,13 @@ public:
     }
     
     bool exist(vector<vector<char>>& board, string word) {
-        
+        int visited[7][7] = {0};
         for(int row = 0; row < board.size(); row ++){
             for(int col = 0; col < board[0].size(); col ++){
                 if(board[row][col] == word[0]){
                     memset(visited, 0, sizeof(visited));
                     visited[row][col] = 1;
-                   bool isFound =  wordSearch(row, col, 1, word, board);
+                   bool isFound =  wordSearch(row, col, 1, word, board, visited);
                     if(isFound){
                         return true;
                     }
