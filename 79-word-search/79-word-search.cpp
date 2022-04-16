363d1043c2e3cdd37e840c381afe95ac44345f01
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-17 03:22:27 +0600

    Time: 343 ms (71.78%), Space: 8 MB (36.51%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,54 +1,51 @@
-
 int fx[] = {0, 0, +1, -1};
 int fy[] = {+1, -1, 0, 0};
-
 class Solution {
 public:
-    
-    
-    
-    bool wordSearch(int row, int col, int charIndex, string word, vector<vector<char>>& board){
+
+
+
+    bool wordSearch(int row, int col, int charIndex, string word, vector<vector<char>>& board ){
         if (charIndex >= word.size()){
             return true;
         }
-        
-         if ( row <  0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[charIndex]){
-                        return false;
-                    }
+
         bool isFound = false;
-         board[row][col] = '*';
+        // board[row][col] = '*';
         for(int index = 0; index < 4; index ++){
             int newRow = fx[index] + row;
             int newCol = fy[index] + col;
-           
-                isFound |= wordSearch(newRow, newCol, charIndex + 1, word, board);
+
+            if ( (newRow >= 0 && newRow < board.size()) && (newCol >= 0 && newCol < board[0].size()) && board[newRow][newCol] == word[charIndex]){
+               board[newRow][newCol] ='*';
+                isFound |= wordSearch(newRow, newCol, charIndex + 1, word, board );
                 if(isFound){
                     return true;
-                }
+                } board[newRow][newCol] = word[charIndex];
             }
-     board[row][col] = word[charIndex];
-    
+        }
+     
         return isFound;
-        
+
     }
-    
+
     bool exist(vector<vector<char>>& board, string word) {
-        
+
         for(int row = 0; row < board.size(); row ++){
             for(int col = 0; col < board[0].size(); col ++){
                 if(board[row][col] == word[0]){
-                   
-                   bool isFound =  wordSearch(row, col, 0, word, board);
-                   
+                    board[row][col] = '*';
+                    bool isFound =  wordSearch(row, col, 1, word, board);
+                    board[row][col] = word[0];
                     if(isFound){
                         return true;
                     }
                 }
             }
         }
-        
+
         return false;
-        
-        
+
+
     }
 };
