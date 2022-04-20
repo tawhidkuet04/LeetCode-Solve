8b4d0d2834065a4c253a035f6d38b3708c6f2a1a
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-21 02:44:39 +0600

    Time: 3 ms (96.54%), Space: 10.8 MB (72.06%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -4,34 +4,22 @@ public:
     
     void makeCombinationSum(int index, vector<int> &candidates, 
                             vector< vector<int> > &ans, 
-                            vector<int> &combination, int sum, int target,
-                            unordered_map<string,int> &mp, string combStr){
+                            vector<int> &combination, int sum, int target){
         if( sum > target || index >= candidates.size()) {
             return;
         }
         
         if( sum == target){
-            if(mp[combStr] == 0){
-                mp[combStr] = 1;
-                ans.push_back(combination);
-            }
-            
+            ans.push_back(combination);
             return;
         }
         
-            combStr += (index + '0');
-            sum += candidates[index];
-            combination.push_back(candidates[index]);
-            makeCombinationSum( index, candidates, ans, combination, sum, target, mp, combStr);
-           if(combination.size() > 0){
-            sum -= candidates[index];
-            combStr.pop_back();
-            combination.pop_back();
-            makeCombinationSum( index + 1, candidates, ans, combination, sum, target,mp, combStr);
-        
-        }
-          
-        
+        sum += candidates[index];
+        combination.push_back(candidates[index]);
+        makeCombinationSum( index, candidates, ans, combination, sum, target);
+        sum -= candidates[index];
+        combination.pop_back();
+        makeCombinationSum( index + 1, candidates, ans, combination, sum, target); 
     }
     
 
@@ -40,9 +28,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector< vector<int> > ans;
         vector<int>combination;
-        unordered_map<string,int>mp;
-        sort(candidates.begin(), candidates.end());
-        makeCombinationSum( 0, candidates, ans, combination , 0, target, mp, "");
+        makeCombinationSum( 0, candidates, ans, combination , 0, target);
         
         return ans;
     }
