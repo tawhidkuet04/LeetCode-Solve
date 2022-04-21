92d4f75eea0b70b75153c00210dc2be92b0ee456
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-21 23:51:32 +0600

    Time: 13 ms (32.72%), Space: 10.9 MB (38.89%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -4,31 +4,29 @@ public:
     
     void findCombinations(int index, vector<int> & candidates, 
                            vector< vector<int> >& ansSet, vector<int> & combination,
-                          int sum, int target){
-       if(sum == target){
+                          int sum){
+       if(sum == 0){
             ansSet.push_back(combination);
             return ;
         }
         
-        if(sum > target || index >= candidates.size()){
+        if(sum < 0 || index >= candidates.size()){
             
             return;
         }
         
         for(int start = index; start < candidates.size(); start ++ ){
-            
+            if( candidates[start] > sum ) break;
             if(start  != index){
                 if( start > 0 && candidates[start - 1] != candidates[start]){
                     combination.push_back(candidates[start]);
-                   findCombinations(start + 1, candidates, ansSet, combination, sum + candidates[start],
-                                 target); 
+                   findCombinations(start + 1, candidates, ansSet, combination, sum - candidates[start]); 
                    combination.pop_back();
                 }
                 
             }else{
               combination.push_back(candidates[start]);
-               findCombinations(start + 1, candidates, ansSet, combination, sum + candidates[start],
-                             target); 
+               findCombinations(start + 1, candidates, ansSet, combination, sum -candidates[start]); 
                combination.pop_back();
             }
         }
@@ -41,7 +39,7 @@ public:
         vector< int > combination;
 
         sort(candidates.begin(), candidates.end());
-        findCombinations(0, candidates, ansSet, combination, 0, target);
+        findCombinations(0, candidates, ansSet, combination, target);
         
 
         return ansSet;
