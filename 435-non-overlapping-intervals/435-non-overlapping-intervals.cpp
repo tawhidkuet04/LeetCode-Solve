3f2656a455d30d0df0bdce3942b688ac238a6634
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-24 03:02:20 +0600

    Time: 759 ms (21.72%), Space: 140.5 MB (8.73%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    
+    bool isFirstInterValBigger(vector<int> first, vector<int> second){
+        
+        if(first[1] > second[1]){
+            return true;
+        }else return false;
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
+        if(intervals.size() == 1){
+            return 0;
+        }
+        
+        sort(intervals.begin(), intervals.end(), [](auto const &first, auto const &second){
+           if(first[0] < second[0]){
+               return true;
+           }else if(first[0] == second[0]){
+               return first[1] > second[1];
+           }else return false;
+        });
+        
+        int firstIndex = 0, secondIndex = 1, size = intervals.size();
+        
+        int ans = 0;
+        
+        while(secondIndex < size){
+             auto firstInterval = intervals[firstIndex];
+             auto secondInterval = intervals[secondIndex];
+             
+            if(firstInterval[1] > secondInterval[0]){
+                ans ++ ;
+                
+                if(isFirstInterValBigger(firstInterval, secondInterval)){
+                    firstIndex = secondIndex;
+                    secondIndex ++;
+                }else{
+                    secondIndex ++;
+                }
+            }else{
+                firstIndex = secondIndex;
+                secondIndex ++;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+// 1 5
+// 2 3 
+// 4 6
+    
+// 1   3
+// 1      5
+//   2 3
+//     3 4
+
+
+// 1 5
+// 1 3
+// 1 2 
+
+// 1 2 
+// 1 15    
+// 3 10
