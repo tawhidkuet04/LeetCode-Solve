acd70f29e1ce2e28bdaa682cb39f35861e70cf07
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-24 01:34:00 +0600

    Time: 58 ms (23.25%), Space: 21.8 MB (5.00%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    
+    vector<int> getOverlapedSegment(vector<int> first, vector<int> second){
+        if(first[0] > second[0]){
+            swap(first, second);
+        }
+        vector<int>overLapped;
+        if(first[1] >= second[0]){
+            int start = min(first[1], second[0]);
+            int end = min(first[1], second[1]);
+            overLapped.push_back(start);
+            overLapped.push_back(end);
+            return overLapped;
+        }else{
+            return overLapped;
+        }
+    }
+    
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        
+        vector< vector<int> > ans;
+        
+        int firstListLength = firstList.size();
+        int secondListLength = secondList.size();
+        
+        if(firstListLength == 0 ||
+          secondListLength == 0){
+            return ans;
+        }
+        
+        
+        int firstListIndex = 0, secondListIndex = 0;
+        
+        
+        while( firstListIndex < firstListLength && 
+              secondListIndex < secondListLength){
+            
+            auto overLapped = getOverlapedSegment(firstList[firstListIndex],
+                                                 secondList[secondListIndex]);
+            if(overLapped.size() != 0){
+                ans.push_back(overLapped);
+            }
+            
+            if(firstList[firstListIndex][1] > secondList[secondListIndex][1]){
+                secondListIndex ++ ;
+            }else{
+                firstListIndex ++;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
