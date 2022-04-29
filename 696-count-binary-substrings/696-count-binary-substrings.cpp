b481d50b54af8cc387d8daf9e9c42bcebe8e927d
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-30 03:33:42 +0600

    Time: 23 ms (81.27%), Space: 10.5 MB (48.99%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -3,23 +3,18 @@ public:
     int countBinarySubstrings(string s) {
         vector<int> arr;
         int size = s.size();
-        int cnt = 0 ;
+        int cnt = 1 , ans  = 0 , prevCount = 0 ;
         for(int index = 1 ; index < size; index ++ ){
             if(s[index] != s[index - 1]){
-                arr.push_back(cnt + 1);
-                cnt = 0;
+               ans += min(prevCount, cnt);
+                prevCount = cnt;
+                cnt = 1;
             }else{
                 cnt ++ ;
             }
         }
-         arr.push_back(cnt + 1);
-       
-        int ans = 0 ;
+        ans += min(prevCount, cnt);
         
-        for(int index = 1; index < arr.size(); index ++ ){
-            cout << arr[index];
-            ans += min(arr[index], arr[index - 1]);
-        }
         return ans;
     }
 };
