83830a031a170366f24c066fd35fac8b0183454b
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-29 16:21:53 +0600

    Time: 135 ms (23.60%), Space: 61.2 MB (70.90%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    int calculatSubArrays(int n){
-        return (n * (n + 1)) - (n * (n + 1))/2;
-    }
-    
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         if(k == 0) return 0;
         int product = 1, windowStart = 0, size = nums.size(), ans = 0 ;
@@ -11,18 +7,16 @@ public:
         for(int windowEnd = 0; windowEnd < size; windowEnd ++ ){
             product *= nums[windowEnd];
             
-            while(product >= k){
+            while(product >= k && windowStart <= windowEnd){
                 product /= nums[windowStart];
                 windowStart ++;
             }
             
             int range = windowEnd - windowStart + 1;
-            int prevRange = range - 1;
-            
-            ans += calculatSubArrays(range) - calculatSubArrays(prevRange);
+            ans += range;
         }
         
-        return (ans < 0 ) ? 0 : ans;
+        return ans;
         
     }
 };
