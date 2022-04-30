7996c9689b431fd0d3d06cbb91995f1d584e71c1
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-04-30 17:20:21 +0600

    Time: 37 ms (88.25%), Space: 28.2 MB (66.41%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
-    int findNum(int lo, int hi, int num, bool isLowerBound, vector<int> & time){
-        int ans = - 1;
-        while(lo <= hi){
-            int mid = lo + ( hi - lo )/2;
-            
-            if( time[mid] > num){
-                hi = mid - 1;
-            }else if (time[mid] < num){
-                lo = mid + 1;
-            }else{
-                 ans = mid;
-                if(isLowerBound){
-                    hi = mid - 1;
-                }else{
-                    lo = mid + 1;
-                }
-            }
-        }
-        return ans;
-    }
-    
+
     
     int numPairsDivisibleBy60(vector<int>& time) {
+        int mp[62] = {0};
+        int ans = 0 ;
         for(int &num : time){
             num %= 60;
+            int numToFind = (60 - num) % 60 ;
+            ans += mp[numToFind];
+            mp[num] ++ ;
             // cout << num << endl;
         }
-        sort(time.begin(), time.end());
         
-        int size = time.size(), ans = 0;
-        
-        
-        for(int index = 0; index < size; index ++){
-            int lowerBound = findNum(index + 1, size - 1, (60 - time[index]) % 60, true, time);
-            if(lowerBound == -1 ){
-                continue;
-            }
-            
-            int upperBound = findNum(index + 1, size - 1, (60 - time[index]) % 60, false, time);
-            
-            ans += (upperBound - lowerBound + 1);
-            
-            
-        }
+       
        
         return ans;
         
@@ -53,3 +23,5 @@ public:
 // 30 20 20 40 30 
     
 //     0 0 0 
+
+// 30 20 30 40 40
