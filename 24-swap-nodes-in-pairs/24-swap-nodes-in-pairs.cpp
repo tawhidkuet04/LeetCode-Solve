4ea1347f5df3f15d6b06e240517521ebad5fcda4
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-04 21:37:17 +0600

    Time: 0 ms (100.00%), Space: 7.7 MB (6.93%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,22 +11,40 @@
 class Solution {
 public:
     
-    ListNode* solve(ListNode *head){
-        if(head == NULL) return head;
-        if(head->next == NULL) return head;
-        
-        ListNode *firstNode = head;
-        ListNode *secondNode = head->next;
-        
-        firstNode->next = solve(secondNode->next);
-        secondNode->next = firstNode;
-        
-        return secondNode;
-        
-    }
-    
+
     ListNode* swapPairs(ListNode* head) {
-        if(head == NULL) return head;
-        return solve(head);
+        if(head == NULL || head->next == NULL) return head;
+        
+      
+        ListNode *firstNode = head, *secondNode;
+        
+        ListNode *startNode = firstNode->next, *prev = new ListNode(-1);
+        
+        while(firstNode != NULL){
+            if(firstNode->next == NULL) return  startNode;
+            
+            secondNode = firstNode->next;
+            ListNode *temp = secondNode->next;
+            
+            prev->next = secondNode;
+            secondNode->next = firstNode;
+            firstNode->next = temp;
+           
+            prev = firstNode;
+            firstNode = firstNode->next;
+            
+//                ListNode *temp = firstNode;
+//                firstNode = secondNode;
+//                secondNode = temp;
+               
+            
+            
+        }
+        
+        // 1 2 3 4
+            
+            
+        
+        return startNode;
     }
 };
