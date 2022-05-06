0f8cf91615439355cde8bc94b2cd72de6858a859
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-07 00:59:03 +0600

    Time: 75 ms, Memory: 24.8 MB - LeetHub

---
+++ group-anagrams/group-anagrams.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string> > mp ;
+        
+        
+        for(int index = 0 ; index < strs.size(); index ++ ){
+            string str = strs[index];
+            int charValArr[26] = {0};
+            
+            for(int charIndex = 0 ; charIndex < str.size(); charIndex ++ ){
+                  charValArr[ str[charIndex] - 'a' ] ++ ;
+            }
+            
+            string hashedKey = "";
+            
+            for(int charIndex = 0 ; charIndex < 26 ; charIndex ++ ){
+                hashedKey += charValArr[charIndex] + '0';
+            }
+            
+            auto mpStringArray =  mp[hashedKey];
+            mpStringArray.push_back(str);
+            mp[hashedKey] = mpStringArray;
+        }
+        vector< vector<string> > ans;
+        
+        for( auto const keyVal: mp){
+            
+            ans.push_back(keyVal.second);
+        }
+        return ans;
+    }
+};
+
+
