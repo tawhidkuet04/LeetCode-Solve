008f08f6836528182d5b8388f53517f8e2d35662
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-12 03:36:24 +0600

    Time: 17 ms (12.28%), Space: 15.3 MB (13.34%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode *root, TreeNode* par, unordered_map<TreeNode*,TreeNode*> &parMp){
+        if(root == NULL) return;
+        
+        parMp[root] = par;
+        
+        dfs(root->left, root, parMp);
+        dfs(root->right, root, parMp);
+        
+    }
+    
+    void bfs(TreeNode *root, int k, unordered_map<TreeNode*,TreeNode*> mp, vector<int> &ans){
+        unordered_map<TreeNode*,int> vis;
+        
+        queue<TreeNode *> q;
+        
+        q.push(root);
+        vis[root] = 1;
+        
+        int distance = 0;
+        
+        while(!q.empty()){
+            
+            int sz = q.size();
+            
+            
+            for(int index = 0; index < sz; index ++){
+                auto top = q.front();
+                q.pop();
+                
+                ans.push_back(top->val);
+                
+                if(top->left != NULL && !vis[top->left]){
+                    q.push(top->left);
+                    vis[top->left] = 1;
+                }
+                if(top->right != NULL && !vis[top->right]){
+                    q.push(top->right);
+                    vis[top->right] = 1;
+                }
+                if(mp.find(top) != mp.end() && !vis[mp[top]] && mp[top] != NULL){
+                    q.push(mp[top]);
+                    vis[mp[top]] = 1;
+                }
+            }
+            if(distance == k){
+                break;
+            }else{
+                ans.clear();
+            }
+            
+            distance ++;
+        }
+        
+        
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*> parMp;
+        
+        dfs(root, NULL, parMp);
+        
+        vector<int> ans;
+        
+        bfs(target, k, parMp, ans);
+        
+        return ans;
+        
+    }
+};
