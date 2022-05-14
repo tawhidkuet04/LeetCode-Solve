5220ca44b06a25f2f60cfd2b04697f699c5a1d81
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-14 16:11:53 +0600

    Time: 20 ms (46.96%), Space: 10.4 MB (7.51%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,33 +1,54 @@
 class Solution {
 public:
     
-    int solve(int i, int j, vector<vector<int>>& matrix, vector< vector<int> > &dp){
+//     int solve(int i, int j, vector<vector<int>>& matrix, vector< vector<int> > &dp){
         
-        if(i >= matrix.size() || j >= matrix[0].size() || j < 0)  return 10000000;
+//         if(i >= matrix.size() || j >= matrix[0].size() || j < 0)  return 10000000;
         
-        if( i == matrix.size() - 1) return matrix[i][j];
+//         if( i == matrix.size() - 1) return matrix[i][j];
         
-        if(dp[i][j] != - 1) return dp[i][j];
+//         if(dp[i][j] != - 1) return dp[i][j];
         
-        int left = matrix[i][j] +  solve(i + 1, j - 1, matrix, dp);
-        int right = matrix[i][j] + solve( i + 1, j + 1, matrix, dp);
-        int below = matrix[i][j] + solve( i + 1, j , matrix, dp);
+//         int left = matrix[i][j] +  solve(i + 1, j - 1, matrix, dp);
+//         int right = matrix[i][j] + solve( i + 1, j + 1, matrix, dp);
+//         int below = matrix[i][j] + solve( i + 1, j , matrix, dp);
         
-        return dp[i][j] = min( left, min(right, below));
+//         return dp[i][j] = min( left, min(right, below));
         
-    }
+//     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int rowSize = matrix.size();
         int colSize = matrix[0].size();
         
-        vector< vector<int> > dp(rowSize, vector<int> (colSize, - 1));
+        vector< vector<int> > dp(rowSize, vector<int> (colSize, 0));
         
         int ans = INT_MAX;
         
-        for(int i = 0 ; i < colSize; i ++ ){
-            ans = min(ans, solve(0, i, matrix, dp));
+//         for(int i = 0 ; i < colSize; i ++ ){
+//             ans = min(ans, solve(0, i, matrix, dp));
+//         }
+        
+        for(int index = 0; index < colSize; index ++ ){
+            dp[0][index] = matrix[0][index];
         }
+        
+        for(int row = 1; row < rowSize; row ++){
+            for(int col = 0 ; col < colSize; col ++ ){
+                int left = INT_MAX, right = INT_MAX, up = INT_MAX;
+                
+               if(col > 0 )left = matrix[row][col] + dp[row - 1][col - 1];
+               if( col < colSize - 1)right = matrix[row][col] + dp[row - 1][col + 1];
+                up = matrix[row][col] + dp[row - 1][col];
+                
+                dp[row][col] = min(left, min(up, right));
+            }
+        }
+        
+        for(int index = 0; index< colSize; index ++ ){
+            ans = min(ans, dp[rowSize - 1][index]);
+        }
+        
         
         return ans;
         
