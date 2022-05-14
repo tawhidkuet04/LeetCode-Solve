d53b40ad52bb215e47702e7c852de18f6245508e
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-15 01:43:06 +0600

    Time: 843 ms (5.12%), Space: 77.1 MB (10.79%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     
-    bool solve(int index, int sum1, vector<int>& nums, int first , int second, int &sum,  vector< vector<int> > &dp){
+    bool solve(int index, int sum1, vector<int>& nums, int first , int second, int &sum,  vector< vector<optional<bool>> > &dp){
         if(sum1 == (sum - sum1) && sum1 != 0 && (first + second) == nums.size()) {
             return true;
         }
         if(index >= nums.size()) return false;
         
-        if(dp[index][sum1] != -1) return dp[index][sum1];
+        if(dp[index][sum1] != nullopt) {
+            return (dp[index][sum1] == true);
+        }
         
         bool takeAndNotTake = solve(index + 1, sum1 + nums[index], nums,  first + 1, second, sum, dp);
         bool notTakeAndTake = solve(index + 1, sum1, nums, first, second + 1, sum, dp);
         
-        return dp[index][sum1] = (takeAndNotTake | notTakeAndTake);
+      dp[index][sum1] = (takeAndNotTake | notTakeAndTake);
+      return (takeAndNotTake | notTakeAndTake);
     }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int sz = nums.size();
         
-        vector< vector<int> > dp(sz, vector<int> (20009, -1));
+       
         for(int index = 0; index < sz; index ++ ){
             sum += nums[index];
         }
+        if(sum % 2 == 1) return false;
+        int subsetSum = sum + 100;
+        vector< vector<optional<bool>> > dp(sz + 1, vector<optional<bool>> (subsetSum, nullopt));
         return solve(0, 0 , nums, 0,0, sum, dp);
     }
 };
