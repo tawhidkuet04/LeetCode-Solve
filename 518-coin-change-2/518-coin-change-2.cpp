9442017484435988d5fbb96916e0b1a72fe4be76
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-15 17:12:28 +0600

    Time: 59 ms (26.53%), Space: 20.9 MB (11.91%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -23,9 +23,34 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int sz = coins.size();
-        vector< vector<int> > dp(sz, vector<int>(amount + 100, -1));
+        vector< vector<int> > dp(sz, vector<int>(amount + 100, 0));
         
-        return solve(0, amount, coins, dp);
+        for(int index = 0; index < sz; index ++ ){
+            dp[index][0] = 1;
+        }
+        
+        for(int target = 1; target <= amount; target ++ ){
+            if( target % coins[0] == 0){
+                 dp[0][target] = 1;
+            }
+           
+        }
+        
+        
+        for (int index = 1; index < sz; index ++ ){
+            for(int target = 0; target <= amount ; target ++ ){
+                int take = 0 ;
+                if( (target - coins[index]) >= 0 ){
+                    take = dp[index][target - coins[index]];
+                }
+                int notTake = dp[index - 1][target];
+                dp[index][target] = take + notTake;
+            }
+        }
+        
+        
+        
+        return dp[sz - 1][amount];
         
         
         
