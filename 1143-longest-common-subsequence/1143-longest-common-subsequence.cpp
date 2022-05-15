9cfa33ddb4b9517d60497b3cdafea35b65a222d8
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-15 22:23:10 +0600

    Time: 34 ms (41.58%), Space: 17.7 MB (10.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -27,30 +27,39 @@ public:
         // text1 = text2;
         // text2 = temp;
         // cout << text1 << " " << text2 << endl;
-        if(text1[0] == text2[0])dp[0][0] = 1;
-        for(int index = 1 ; index < text2.size(); index ++ ){
-            if(text1[0] == text2[index]){
-                 dp[0][index] = 1;
-            }else{
-                dp[0][index] = max(dp[0][index - 1], 0);
-            }
-        }
-        for(int index = 1 ; index < text1.size(); index ++ ){
-            if(text1[index] == text2[0]){
-                 dp[index][0]  = 1;
-            }else{
-              dp[index][0] = max(dp[index - 1][0], 0);
-            }
-        }
+        // if(text1[0] == text2[0])dp[0][0] = 1;
+        // for(int index = 1 ; index < text2.size(); index ++ ){
+        //     if(text1[0] == text2[index]){
+        //          dp[0][index] = 1;
+        //     }else{
+        //         dp[0][index] = max(dp[0][index - 1], 0);
+        //     }
+        // }
+        // for(int index = 1 ; index < text1.size(); index ++ ){
+        //     if(text1[index] == text2[0]){
+        //          dp[index][0]  = 1;
+        //     }else{
+        //       dp[index][0] = max(dp[index - 1][0], 0);
+        //     }
+        // }
         
-        for(int index1 = 1; index1 < text1.size();  index1 ++ ){
-            for(int index2 = 1; index2 < text2.size(); index2 ++ ){
-                int take =  dp[index1 - 1][index2 - 1],takeFirst = -1e8, takeSecond = -1e8;
+        for(int index1 = 0; index1 < text1.size();  index1 ++ ){
+            for(int index2 = 0; index2 < text2.size(); index2 ++ ){
+                int take = 0 ,takeFirst = 0, takeSecond = 0;
                 if(text1[index1] == text2[index2]){
-                    take = 1 + dp[index1 - 1][index2 - 1];
+                    if( index1 - 1 >= 0 && index2 - 1 >= 0){
+                        take = 1 + dp[index1 - 1][index2 - 1];
+                    }else{
+                        take = 1 ;
+                    }
+                    
                 }else{
-                    takeFirst = dp[index1 - 1][index2];
+                    if(index1 - 1 >= 0){
+                         takeFirst = dp[index1 - 1][index2];
+                    }
+                    if(index2 - 1 >= 0){
                     takeSecond = dp[index1][index2 - 1];
+                    }
                 }
                dp[index1][index2] = max(take, max(takeFirst, takeSecond));
             }
