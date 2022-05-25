836f387908b8c54e13fb985355ff347359576cc0
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-25 07:28:20 +0600

    Time: 19 ms (59.10%), Space: 10.7 MB (48.66%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
+    struct trip{
+        int passenger;
+        int distance;
+        bool isStart;
+        
+        trip(int passenger, int distance, bool isStart){
+            this->passenger = passenger;
+            this->distance = distance;
+            this->isStart = isStart;
+        }
+    };
+    
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-       vector< int > path(1001, 0);
+         vector< trip > arr ;
         
-        int sz = trips.size();
-        
-        int mx = 0 ;
-        
-        for(int index = 0; index < sz; index ++ ){
+        for(int index = 0; index < trips.size(); index ++ ){
             int from = trips[index][1];
             int to = trips[index][2];
             int passenger = trips[index][0];
             
-            path[from] += passenger;
+            arr.push_back(trip(passenger, from, true));
+            arr.push_back(trip(passenger, to, false));
+        }
+        
+        sort(arr.begin(), arr.end(), [] (const trip &first, const trip &second) -> bool{
+            if(first.distance < second.distance) return true;
+            else if(first.distance ==  second.distance){
+                return first.isStart < second.isStart;
+            }else {
+                return false;
+            }
+        }) ;
+        
+        int total = 0 ;
+        
+        for(int index = 0; index < arr.size(); index ++ ){
+           
+            auto curTrip = arr[index];
+             // cout << curTrip.passenger << " " << curTrip.distance << endl;
+             
+            if(curTrip.isStart){
+                total +=  curTrip.passenger;
+            }else{
+                 total -= curTrip.passenger;
+            }
+            // cout << total << endl;
+            if(total > capacity ) return false;
             
-            path[to] -= passenger;
-                
-            mx = max(mx, to);
         }
         
-        int total = 0;
-        
-        for(int index = 0; index <= mx; index ++ ){
-            total += path[index];
-            if(total > capacity) return false;
-        }
         return true;
     }
+    
 };
