f70939fe6c7690c4604a039837b7557bccae59eb
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-30 05:50:53 +0600

    Time: 39 ms (64.66%), Space: 10.4 MB (87.55%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    
+    int getLisIndex( vector<int> &lis, int num){
+        int lo = 0, hi = lis.size() - 1;
+        while(lo <= hi){
+            int mid = (lo + (hi-lo)/2);
+            
+            if(lis[mid] == num){
+                return mid;
+            }else if (lis[mid] < num){
+                 lo = mid + 1;
+            }else{
+                 hi = mid - 1;
+            }
+        }
+        return lo;
+    }
+    
+    
+    // 1 2 4 6 9
+    
+    int lengthOfLIS(vector<int>& nums) {
+         vector<int> lis;
+         int sz = nums.size();
+         int lisIndex = 0;
+         for(int index = 0; index < sz; index ++ ){
+              // cout << lisIndex << " "<< lis.size() << endl;
+             if(lisIndex == 0){
+                 lis.push_back(nums[index]);
+                 lisIndex ++ ;
+             }else{
+                // cout << lis[0] << endl;
+                 if(lis[lisIndex - 1] < nums[index]){
+                     lis.push_back(nums[index]);
+                     lisIndex ++;
+                     // cout << lisIndex << endl;
+                 }else{
+                     int insertIndex = getLisIndex(lis, nums[index]);
+                     cout << insertIndex << endl;
+                     lis[insertIndex] = nums[index];
+                 }
+                 
+             }
+         }
+        
+        return lis.size();
+
+    }
+};
