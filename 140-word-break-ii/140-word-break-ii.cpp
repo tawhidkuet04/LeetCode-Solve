a05bb7a89349e090dd42d8d6d2a62c9a46a84cf8
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-05-29 07:25:28 +0600

    Time: 3 ms (44.42%), Space: 7.3 MB (41.36%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -3,46 +3,37 @@ public:
     
 
     
-     void solve(int index,string s, vector<string> &ans, string sentence, unordered_map<string, int>& mp, vector<int> & dp){
-        // cout << index << endl;
+     void solve(int index,string s, vector<string> &ans, string sentence, unordered_map<string, int>& mp){
         if(index >= s.size()){
             sentence.pop_back();
             ans.push_back(sentence);
             return;
-            // return true;
         }
-        
-        
-        // if(dp[index] != -1) return dp[index];
-        // bool flag = false;
-        
         
         for(int start = index ; start < s.size(); start ++ ){
               string str = s.substr(index, start - index + 1);
-              // cout << str << endl;
               if(mp[str]){
                   // if(sentence.size() == 0){
                   //     sentence += str;
                   // }else{
                   //     sentence += " " + str;
                   // }
-                    solve(start + 1, s, ans, sentence + str + " " ,mp, dp);
-                  // flag = max(flag, newFlag);
+                    solve(start + 1, s, ans, sentence + str + " " , mp);
               }
         }
-        
-         // return  dp[index] = flag;
+
     }
     
      vector<string> wordBreak(string s, vector<string>& wordDict) {
          unordered_map<string, int> mp;
-         vector<int> dp(s.size(), -1);
+         
          vector<string> ans;
+         
          for(auto &str: wordDict){
              mp[str] = 1;
          }
         
-         solve(0,s, ans, "", mp, dp);
+         solve(0,s, ans, "", mp);
          return ans;
     }
 };
