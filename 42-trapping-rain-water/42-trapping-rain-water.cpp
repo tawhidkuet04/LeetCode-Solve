c47ea3a9b5ec778d93252013afdaf57e7df1b87f
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-06-07 07:24:43 +0600

    Time: 114 ms (5.41%), Space: 20.4 MB (13.95%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int ans = 0, size = height.size(), left = 0, right = size - 1,
-        leftMax = 0, rightMax = 0 ;
-        
-        while(left <= right){
-            if(height[left] <= height[right]){
-                if(leftMax > height[left]){
-                    ans += leftMax - height[left];
-                }else{
-                    leftMax = height[left];
-                }
-                left ++;
-            }else{
-                 if(rightMax > height[right]){
-                    ans += rightMax - height[right];
-                }else{
-                    rightMax = height[right];
-                }
-                right --;
-            }
-            
+ if(height.empty())
+        return 0;
+    int ans = 0;
+    int size = height.size();
+    vector<int> left_max(size), right_max(size);
+    left_max[0] = height[0];
+    for (int i = 1; i < size; i++) {
+        left_max[i] = max(height[i], left_max[i - 1]);
+    }
+    right_max[size - 1] = height[size - 1];
+    for (int i = size - 2; i >= 0; i--) {
+        right_max[i] = max(height[i], right_max[i + 1]);
+    }
+    for (int i = 1; i < size - 1; i++) {
+        cout << left_max[i-1] << " " << right_max[i + 1]<< endl;
+        if(min(left_max[i-1], right_max[i + 1]) - height[i] >= 0){
+             ans += min(left_max[i-1], right_max[i + 1]) - height[i]; 
         }
-        
-        return ans;
+       
+    }
+    return ans;
         
     }
 };
