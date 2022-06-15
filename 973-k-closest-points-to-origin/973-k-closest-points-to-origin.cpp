b76a84a5f0b1b5ffbcd08c8cfad443b27355dc00
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-06-15 21:45:38 +0600

    Time: 338 ms (54.20%), Space: 60.5 MB (79.88%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,33 +1,58 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>> maxPQ;
-        for (int i = 0 ; i < points.size(); i++) {
-            pair<int, int> entry = {squaredDistance(points[i]), i};
-            if (maxPQ.size() < k) {
-                // Fill the max PQ up to k points
-                maxPQ.push(entry);
-            } else if (entry.first < maxPQ.top().first) {
-                // If the max PQ is full and a closer point is found,
-                // discard the farthest point and add this one
-                maxPQ.pop();
-                maxPQ.push(entry);
+    
+    // 5 6 23 3 6 1 10
+    
+    int getDistance(vector<int> &point){
+        return point[0] * point[0] + point[1] * point[1];
+    }
+    
+    int findPivot(int lo, int hi, vector<vector<int>>& points){
+        int pivotIndex = lo + rand() % (hi - lo + 1);
+       
+        
+        swap(points[hi], points[pivotIndex]);
+        
+        int targetDistance = getDistance(points[hi]);
+        
+        pivotIndex = lo;
+        
+        for(int index = lo; index < hi; index ++ ){
+            if(getDistance(points[index]) <= targetDistance){
+                swap(points[index], points[pivotIndex ++ ]);
             }
         }
         
-        // Return all points stored in the max PQ
-        vector<vector<int>> answer;
-        while (!maxPQ.empty()) {
-            int entryIndex = maxPQ.top().second;
-            maxPQ.pop();
-            answer.push_back(points[entryIndex]);
-        }
-        return answer;
+        swap(points[pivotIndex], points[hi]);
+       
+        return pivotIndex;
     }
-
-private:
-    int squaredDistance(vector<int>& point) {
-        // Calculate and return the squared Euclidean distance
-        return point[0] * point[0] + point[1] * point[1];
+    
+    void quickSelect(int lo, int hi, vector<vector<int>>& points, int k){
+         if(lo < hi){
+             int pivot = findPivot(lo, hi, points);
+            cout << pivot << endl;
+             if( pivot == k){
+                 return ;
+             }else if(pivot < k){
+                 quickSelect(pivot + 1, hi, points, k);
+             }else{
+                  quickSelect(lo, pivot - 1, points, k);
+             }
+         }
+        
+    }
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+         vector<vector<int>> ans;
+        
+         quickSelect(0, points.size() - 1, points, k);
+        
+        
+        for(int index = 0; index < k; index ++ ){
+            ans.push_back(points[index]);
+        }
+        
+        return ans;
     }
 };
