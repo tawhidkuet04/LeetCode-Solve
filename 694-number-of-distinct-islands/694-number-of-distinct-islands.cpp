dd282640350d6aa9c323602a38c1401b5918e195
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-06-17 16:10:07 +0600

    Time: 89 ms (31.04%), Space: 27.2 MB (73.60%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int fx[4] = {0,0,1,-1};
     int fy[4] = {1,-1,0,0};
+    char dir[4] = {'r', 'l', 'd', 'u'};
     int rowSize, colSize;
     
     
@@ -17,47 +18,23 @@ public:
     }
     
     void dfs( int row, int col, vector<vector<int>>& grid,
-             vector<vector<int>>& vis,
-             vector< pair<int,int> > & points,
-             int &minRow,
-             int &minCol){
-        
-        minRow = min(minRow, row);
-        minCol = min(minCol, col);
-        
+             vector<vector<int>>& vis, string &hash){
+
         vis[row][col] = 1 ;
-        points.push_back({row, col});
-        
+    
         for(int index = 0; index < 4; index ++ ){
             int newRow = row + fx[index];
             int newCol = col  + fy[index];
             if(isValid(newRow, newCol, grid, vis)){
-                dfs(newRow, newCol, grid, vis, points, minRow, minCol);
+                hash += dir[index];
+                dfs(newRow, newCol, grid, vis, hash);
             }
         }
+        hash += 'e';
         
         
     }
     
-    
-    string generateHash( vector< pair<int,int> > & points){
-        string hash = "";
-        
-        for(int index = 0; index < points.size(); index ++ ){
-            hash +=  to_string(points[index].first);
-             hash +=  to_string(points[index].second);
-            hash += '.';
-        }
-        
-        return hash;
-    }
-    void normalizePoints( vector< pair<int,int> > & points, int &minRow, int &minCol){
-        for(int index = 0; index < points.size(); index ++ ){
-            points[index].first -= minRow;
-            points[index].second -= minCol;
-        }
-         sort(points.begin(), points.end());
-    }
     
     int numDistinctIslands(vector<vector<int>>& grid) {
         rowSize = grid.size();
@@ -69,12 +46,9 @@ public:
         for(int row = 0; row < rowSize; row ++ ){
             for(int col = 0; col < colSize; col ++ ){
                 if(!vis[row][col] && grid[row][col]){
-                    vector< pair<int,int> > points;
-                    int minRow = 1e9;
-                    int minCol = 1e9;
-                    dfs(row, col, grid, vis, points, minRow, minCol);
-                    normalizePoints(points, minRow, minCol);
-                    st.insert(generateHash(points));
+                    string hash = "s";
+                    dfs(row, col, grid, vis, hash);
+                    st.insert(hash);
                 }  
             }
         }
