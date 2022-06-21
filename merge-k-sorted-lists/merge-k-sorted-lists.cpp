6637182a467570459f3e9e449165b47e48d59b4d
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-06-22 00:26:54 +0600

    Time: 47 ms, Memory: 13.4 MB - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -3,7 +3,7 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}https://leetcode.com/problemset/all/
+ *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
@@ -11,47 +11,57 @@
 class Solution {
 public:
     
-    // 1  2 - 4
-    // 2 4 - 5 
-    
-     ListNode* mergeTwoList( ListNode *first, ListNode *second){
-        ListNode *prevNode = new ListNode(-1);
-         
-        ListNode *prev = prevNode;
-         
+    ListNode *mergeTwoList(ListNode *first, ListNode *second){
+        
+        ListNode *dummy = new ListNode(-1);
+        
+        ListNode *head = dummy;
+        
         while(first != NULL && second != NULL){
             if(first->val < second->val){
-                prevNode->next = first;
+                dummy->next = first;
+                dummy = first;
                 first = first->next;
             }else{
-                prevNode->next = second;
+                dummy->next = second;
+                dummy = second;
                 second = second->next;
             }
-            prevNode = prevNode->next;
         }
-         
-       prevNode->next = (first == NULL) ? second : first;
-         
-        return prev->next;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-         int size = lists.size();
-        if(size == 0) return NULL;
-        if(size <= 1) return lists[0];
-        int interval = 1;
-        while(interval < size){
-             for(int index = 0; index < (size - interval); index += (interval * 2)){
-                 lists[index] = mergeTwoList(lists[index], lists[index + interval]);
-            }
-            
-            interval *= 2;
         
+        while(first != NULL){
+            dummy->next = first;
+            dummy = first;
+            first = first->next;
         }
-       
-        ListNode *ans = lists[0];
-        return ans;
+        
+         while(second != NULL){
+            dummy->next = second;
+            dummy = second;
+            second = second->next;
+        }
+        
+        return head->next;
         
     }
     
-    // 1 2 3 4 5
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        if(lists.size() == 0) return NULL;
+        int index = 0, power = 1, sz = lists.size(), k = 2;
+        
+        while(true){
+            if(index == 0 && (index + power) >= sz) break;
+            for(int index = 0; index < sz; index += k){
+                if(index + power >= sz) break;
+                lists[index] = mergeTwoList(lists[index], lists[index + power]);
+            }
+            
+            k *= 2;
+            power *= 2;
+            
+        }
+        
+        return lists[0];
+    }
 };
