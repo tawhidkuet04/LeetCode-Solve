76203af6d551ec347b76e9ae2a5f90c2c617a1eb
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-06-25 16:32:18 +0600

    Time: 85 ms, Memory: 19 MB - LeetHub

---
+++ word-break/word-break.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    bool solve(int index,string s, unordered_map<string, int>& mp, vector<int> & dp){
+        // cout << index << endl;
+        if(index >= s.size()){
+            
+            return true;
+        }
+        
+        
+        if(dp[index] != -1) return dp[index];
+        bool flag = false;
+        
+        
+        for(int start = index ; start < s.size(); start ++ ){
+              string str = s.substr(index, start - index + 1);
+              // cout << str << endl;
+              if(mp[str]){
+                  bool newFlag = solve(start + 1, s, mp, dp);
+                  flag = max(flag, newFlag);
+              }
+        }
+        
+         return  dp[index] = flag;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+         unordered_map<string, int> mp;
+         vector<int> dp(s.size(), -1);
+        
+         for(auto &str: wordDict){
+             mp[str] = 1;
+         }
+        
+        return solve(0,s, mp, dp);
+    }
+};
