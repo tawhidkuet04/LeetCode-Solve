fe6d9ebe475be95cb5bccb18cac8e8d56372a691
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-06-25 16:45:16 +0600

    Time: 473 ms, Memory: 80.8 MB - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -59,13 +59,47 @@ public:
         
     }
     
+    int solve3(vector<int>& coins, int amount){
+        int sz = coins.size();
+        vector< vector<int> > dp(sz, vector<int>(amount + 10, -1));
+        
+        for(int limit = 0; limit <= amount ; limit ++ ){
+            if(limit % coins[0] == 0){
+                 dp[0][limit] = limit / coins[0];
+            }else{
+                 dp[0][limit] = 1e8;
+            }
+           
+        }
+        
+        for(int index = 1; index < sz; index ++ ){
+            for(int limit = 0; limit <= amount ; limit ++ ){
+                int take = 1e8, notTake = 1e8;
+                
+                if( (limit - coins[index]) >= 0){
+                    take = 1 + dp[index][limit - coins[index]];
+                }
+                notTake = dp[index - 1][limit];
+                dp[index][limit] = min(take, notTake);
+            }
+        }
+        
+        if(dp[sz - 1][amount] == 1e8) return -1;
+        return dp[sz - 1][amount];
+        
+        
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
         vector< vector<int> > dp(coins.size(), vector<int> (amount + 1, -1));
         int ans =   solve(0, coins, amount, 0, dp);
         
-        return solve2(coins, amount);
+        return solve3(coins, amount);
+        
+        
+//         return solve2(coins, amount);
 
-        return (ans == 1e7) ? -1 : ans;
+//         return (ans == 1e7) ? -1 : ans;
         
     }
 };
