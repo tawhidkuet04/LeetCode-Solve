e74c7466071a35666d01f7f36e28d26c50eea696
tawhidkuet04 <tawhidkuet04@gmail.com>
2022-09-27 00:23:25 +0600

    Time: 65 ms (5.76%), Space: 11.7 MB (28.43%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,92 @@
+class Solution {
+public:
+    
+    vector<int> graph[26];
+    int vis[26];
+    
+    pair<char, char> parseString(string &str){
+        return {str[0], str[3]};
+    }
+    
+    bool isEqual(string &str){
+        if(str[1] == '=' && str[2] == '='){
+            return true;
+        }else return false;
+    }
+    
+    
+    bool isPresent(int node, int val){
+        
+        if(node == val) return true;
+        vis[node] = 1;
+        
+        bool flag = false, flagB = false;;
+        for(int index = 0; index < graph[node].size(); index ++){
+            int nodeB = graph[node][index];
+            if(!vis[nodeB]){
+                flagB |= isPresent(nodeB, val);
+            }
+        }
+            return flagB;
+            
+    }
+    
+   void clearVisArr(){
+       for(int index = 0; index < 26; index ++ ){
+           vis[index ] = 0;
+       }
+   }
+            
+            
+    bool equationsPossible(vector<string>& equations) {
+       
+        
+        vector< pair<char,char> > arr;
+        
+        
+        
+        for(int index = 0; index < equations.size(); index ++ ){
+             auto pairChar = parseString(equations[index]);
+            
+            if(isEqual(equations[index])){
+              
+                int nodeA = (int)(pairChar.first - 'a');
+                int nodeB = (int)(pairChar.second - 'a');
+                
+                graph[nodeA].push_back(nodeB);
+                graph[nodeB].push_back(nodeA);
+                
+                // cout << nodeA << " "<< nodeB << endl;
+                
+            }else{
+                arr.push_back(pairChar);
+            }
+        }
+        
+        for(int index = 0; index < arr.size(); index ++ ){
+             auto pairChar = arr[index];
+              int nodeA = (int)(pairChar.first - 'a');
+                int nodeB = (int)(pairChar.second - 'a');
+                
+            
+            for(int index = 0; index < 26; index ++ ){
+                  clearVisArr();
+                 if(isPresent(index,nodeA)){
+                      clearVisArr();
+                     cout << (char)(index + 'a') << " " <<  (char)(nodeB + 'a')<< endl;
+                     if(isPresent(index,nodeB)){
+                         
+                         return false;
+                     }
+                 }
+            }
+           
+        }
+        
+        return true;
+        
+    }
+};
+
+
+
