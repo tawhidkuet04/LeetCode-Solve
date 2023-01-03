4cce4314c05e57d8d51e1132ef8b0a07f6228560
tawhidkuet04 <tawhidkuet04@gmail.com>
2023-01-03 22:07:02 +0600

    Time: 24 ms, Memory: 11.1 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-
-      
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> mp;
-        for(int i = 0 ; i < nums1.size(); i ++ ){
-            mp[nums1[i]]++;
+        unordered_map<int,int> nums1Map, nums2Map;
+        
+        for(int val: nums1){
+            nums1Map[val] ++ ;
         }
+        
         vector<int> ans;
-        for(int i = 0; i < nums2.size(); i ++){
-            if(mp[nums2[i]] > 0){
-                ans.push_back(nums2[i]);
-                mp[nums2[i]]--;
+        
+        
+        for(int val: nums2){
+            nums2Map[val] ++ ;
+            
+            if(nums1Map[val] > 0){
+                nums2Map[val] -- ;
+                nums1Map[val] -- ;
+                ans.push_back(val);
             }
         }
+        
         return ans;
     }
 };
